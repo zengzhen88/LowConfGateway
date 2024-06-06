b46919573fbc1a3a6b7d924af6f5c612f9d9eebc
zengzhen <xingtian_zeng@163.com>
2024-06-06 20:40:23 +0800

    处理上一次git失败造成文件丢失的情况

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -5,6 +5,8 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <inttypes.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
@@ -12,41 +14,67 @@
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_system.h"
+#include <mqtt.h>
+#include <wifi.h>
 
-void app_main(void)
-{
-    printf("Hello world!\n");
+typedef struct {
+    void *wifi;
+    void *mqtt;
+} Gateway;
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+int32_t appPrint(void *priv, const char *strings) {
+    return printf ("%s", strings);
+}
 
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
+void app_main(void) {
+    Gateway *gateway = (Gateway *) malloc (sizeof(*gateway));
+    if (gateway) {
+        memset(gateway, 0x0, sizeof(*gateway));
+
+        {
+            /*wifi*/
+            WifiConfig config;
+            memset(&config, 0x0, sizeof(config));
+
+            strcpy(config.ssid, "TP-LINK_342B");
+            strcpy(config.password, "88888888");
+
+            WifiInitLog(gateway, appPrint);
+            WifiSetLogLevel(LogWifi_Info);
+
+            gateway->wifi = WifiInit(&config);
+        }
+
+        {
+            /*mqtt*/
+            MQTTConfig config;
+            memset(&config, 0x0, sizeof(config));
+
+            MQTTInitLog(gateway, appPrint);
+            MQTTSetLogLevel(LogMQTT_Info);
+
+            strcpy(config.name, "Pushlish");
+            strcpy(config.username, "admin");
+            strcpy(config.password, "123456");
+            strcpy(config.url, "mqtt://192.168.0.106:1883");
+            strcpy(config.hostname, "192.168.0.106");
+            config.port = 1883;
+
+            gateway->mqtt = MQTTInit(&config);
+
+            MQTTSubscribe(gateway->mqtt, "zengzhen", 1);
+        }
+
+        {
+            int running = 0;
+            while (1) {
+                char buf[128];
+
+                vTaskDelay(pdMS_TO_TICKS(2000));
+
+                snprintf (buf, sizeof(buf) - 1, "running_%d", running++);
+                MQTTPushlish(gateway->mqtt, buf, strlen(buf), "zengzhen", 1, 0);
+            }
+        }
     }
-
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
-
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
 }
